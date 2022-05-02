d5f39befeadc2927d6fee992d22a43c4acf7e52c
simon2706 <simonstankoski997@gmail.com>
2022-05-02 11:40:51 +0200

    Restructured the script using Bojan's code.

--- openFaceVideo.py
+++ openFaceVideo.py
@@ -1,14 +1,8 @@
 import os
-import sys
 import random
-import pandas as pd
-
-from pathlib import Path
-path = Path(os.path.abspath(""))
-sys.path.append(str(path))
+from csv import writer
 
 from play_video import play_video
-from csv import writer
 
 
 def append_list_as_row(file_name, list_of_elem):
@@ -19,35 +13,63 @@ def append_list_as_row(file_name, list_of_elem):
         # Add contents of list as last row in the csv file
         csv_writer.writerow(list_of_elem)
 
-a = 1
-# how to get subfolder of current folder
-TaskC = os.path.dirname('.\\TaskC')
-p = os.getcwd()
-print(p)
-os.chdir(p+os.sep+'TaskC')
-p1 = p + os.sep + 'TaskC'
-# how to get all subfolders of current folder
-TaskC_subfolders = [x[0] for x in os.walk(TaskC)][1:]
-random.shuffle(TaskC_subfolders)
-print(TaskC_subfolders)
-append_list_as_row(p1+'/TaskC_data.csv',['path','arousal','valence'])
-for folder in TaskC_subfolders:
-    os.chdir(folder)
+
+def get_input():
+    values = list()
+
+    while True:
+        arousal = int(input('Value for arousal from 1 to 9: '))
+        if arousal in range(1, 9):
+            values.append(arousal)
+            break
+        print('Wrong value')
+
+    while True:
+        valence = int(input('Value for valence from 1 to 9: '))
+        if valence in range(1, 9):
+            values.append(valence)
+            break
+        print('Wrong value')
+
+    return values
+
+
+def get_dir_specific(path):
+    os.chdir(path)
+    directories = os.listdir()
+    for el in directories:
+        if el == 'TaskC':
+            print('TaskC folder exists')
+            os.chdir(path + os.sep + el)
+            break
+        else:
+            print('TaskC folder not found in ' + path)
+            exit()
+
+
+def get_subfolders():
+    subfolders = [x[0] for x in os.walk(os.getcwd())][1:]
+    random.shuffle(subfolders)
+    print(subfolders)
+    return subfolders
+
+
+def work_folders(subfolders):
+    for folder in subfolders:
+        os.chdir(folder)
+        print(os.getcwd())
+        for file in os.listdir():
+            play_video(file, 'timestamps.txt')
+            values = get_input()
+            print(values)
+            path = os.getcwd() + os.sep + file
+            row_content = [path, values[0], values[1]]
+            append_list_as_row('TaskC_data.csv', row_content)
+            print(path)
+        os.chdir('..')
+
+
+if __name__ == "__main__":
     print(os.getcwd())
-    for file in os.listdir():
-        play_video(file, 'timestamps.txt')
-        #check if is not 1-9 number
-        while True:
-            arousal = int(input('Value for arousal from 1 to 9: '))
-            if arousal in range(1,9):
-                break
-        while True:
-            valence = int(input('Value for valence from 1 to 9: '))
-            if valence in range(1,9):
-                break
-        path = os.getcwd() + '/' + file
-        row_content = [path, arousal, valence]
-        append_list_as_row(p1+'/TaskC_data.csv', row_content)
-        print(path)
-    os.chdir('..')
-    #print(os.getcwd())
+    get_dir_specific(os.getcwd())
+    work_folders(get_subfolders())
