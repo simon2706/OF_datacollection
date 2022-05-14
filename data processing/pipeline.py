b86e45af853d69504866dcf04afb5424c1f46361
kiprijanovska <78813492+kiprijanovska@users.noreply.github.com>
2022-05-14 19:04:09 +0200

    code for finding the Frame# of the synchronization peak

--- data processing/pipeline.py
+++ data processing/pipeline.py
@@ -1,8 +1,26 @@
 # -*- coding: utf-8 -*-
 
 from libs.load_data import load_data
+import numpy as np
 
 # insert path/name of the csv file
-path_to_csv = r"TaskA\2022-04-28T12-31-47.csv"
+path_to_csv = r"C:\Users\kipri\OneDrive\Documents\EmteqLabs\EmteqVR Open Face System\Upload\Participant 01\TaskA\2022-04-28T12-31-47.csv"
 
 df = load_data(path_to_csv)
+
+# calculate accelerometer magnitude signal
+df['accelerometer_magnitude'] = np.sqrt(np.square(df['Imu/Accelerometer.x']) +
+                                        np.square(df['Imu/Accelerometer.y']) +
+                                        np.square(df['Imu/Accelerometer.z']))
+
+# plot accelerometer magnitude signal
+df['accelerometer_magnitude'].plot(figsize=(15, 3))
+
+# if you need to zoom in on the signal, use .iloc
+df['accelerometer_magnitude'].iloc[0:100000].plot(figsize=(15, 3))
+
+# find maximum peak in the accelerometer magnitude signal
+max_acc_mag = df['accelerometer_magnitude'].max()
+
+# find the Frame# corresponding to the maximum peak
+frame = df.loc[df['accelerometer_magnitude'] == max_acc_mag]['Frame#'].iloc[0]
